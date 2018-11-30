2c584e9c514b8c6b878e3ba16ddf22a306cbef5b
Jonatas Daniel <jonatas.hermann@gmail.com>
2018-11-30 16:44:53 -0200

    add experience details

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'jonatas.hermann@gmail.com',
+    password:             '',
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
